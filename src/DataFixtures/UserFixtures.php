aae747be52e990c02f3c9ac9b7ef5add0d0622a7
stefano-perico <wap58@mail.com>
2019-09-02 14:28:53 +0200

    installation de composer form validator, faire un composer update

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
