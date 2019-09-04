3e878d8e95c2be1bcf3e60b3624f6ddd5f61b44c
PrinceDeBoulogne <hugo.christin@gmail.com>
2019-09-04 12:35:31 +0200

    cinq articles homepage et css table cheese

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(ArticleRepository $articleRepository)
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepository->findFiveLastArticle(),
         ]);
     }
 }
