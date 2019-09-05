37be7c450328956111d51c31208974a055777335
stefano-perico <wap58@mail.com>
2019-09-05 11:59:39 +0200

    FAIRE UN COMPOSER INSTALL. Modification du service UploaderHelper (ajout d'une const pour la route des images d'article). Création d'un service Twig pour créer un nouveau filtre afin d'atteindre la base du site et afficher les images uploadées. Installation de liip/imagine-bundle pour créer des thumbnail et réduire la taille des images.

--- src/service/UploaderHelper.php
+++ src/service/UploaderHelper.php
@@ -5,20 +5,26 @@ namespace App\service;
 
 
 use Gedmo\Sluggable\Util\Urlizer;
+use Symfony\Component\Asset\Context\RequestStackContext;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UploaderHelper
 {
+	const ARTICLE_IMAGE = 'article_image';
+
 	private $uploadsPath;
 
-	public function __construct(string $uploadsPath)
+	private $requestStackContext;
+
+	public function __construct(string $uploadsPath, RequestStackContext $requestStackContext)
 	{
 		$this->uploadsPath = $uploadsPath;
+		$this->requestStackContext = $requestStackContext;
 	}
 
 	public function uploadArticleImage(UploadedFile $uploadedFile): string
 	{
-		$destination = $this->uploadsPath.'/article_image';
+		$destination = $this->uploadsPath.'/'.self::ARTICLE_IMAGE;
         $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
         $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid().'.'.$uploadedFile->guessExtension();
         $uploadedFile->move(
@@ -27,6 +33,12 @@ class UploaderHelper
         );
 
         return $newFilename;
+	}
+
+	public function getPublicPath(string $path)
+	{
+		return $this->requestStackContext
+			->getBasePath().'/uploads/'.$path;
 	}
 
 }
