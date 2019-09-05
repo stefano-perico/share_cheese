b418f0b812741bbaca0d3ea9be4239ecee86160c
stefano-perico <wap58@mail.com>
2019-09-05 12:00:24 +0200

    FAIRE UN COMPOSER INSTALL. Modification du service UploaderHelper (ajout d'une const pour la route des images d'article). Création d'un service Twig pour créer un nouveau filtre afin d'atteindre la base du site et afficher les images uploadées. Installation de liip/imagine-bundle pour créer des thumbnail et réduire la taille des images.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
